2e2d4b7aa51c25d4142f79aacf681c003382b624
U-UZ\litl <litl@UPC1561.uz.local>
2018-04-02 10:49:26 +0800

    使用maven集成paysdk

--- proj.android-studio/Cocos2dxBridge/src/com/mob/paysdk/cocos2dx/OnPayListener.java
+++ proj.android-studio/Cocos2dxBridge/src/com/mob/paysdk/cocos2dx/OnPayListener.java
@@ -1,29 +1,30 @@
 package com.mob.paysdk.cocos2dx;
 
 import com.mob.paysdk.MobPayAPI;
+import com.mob.paysdk.Order;
 import com.mob.paysdk.PayResult;
 
 /**
  * OnPayListener for cocos2dx.<br/>
  */
-public class OnPayListener<T> extends JavaCxxObject implements com.mob.paysdk.OnPayListener<T> {
+public class OnPayListener extends JavaCxxObject implements com.mob.paysdk.OnPayListener {
 
 	public OnPayListener() {
 		super();
 	}
 
-	@Override
-	public boolean onWillPay(String s, T t, MobPayAPI mobPayAPI) {
-		int cxxThis = getCxxObject();
-		return nativeOnWillPay(cxxThis, s, t, mobPayAPI);
-	}
+//	@Override
+//	public boolean onWillPay(String s, Order t, MobPayAPI mobPayAPI) {
+//		int cxxThis = getCxxObject();
+//		return nativeOnWillPay(cxxThis, s, t, mobPayAPI);
+//	}
 
 	@Override
-	public void onPayEnd(PayResult payResult, T t, MobPayAPI mobPayAPI) {
+	public void onPayEnd(PayResult payResult, Order t, MobPayAPI mobPayAPI) {
 		int cxxThis = getCxxObject();
 		nativeOnPayEnd(cxxThis, payResult.ordinal(), t, mobPayAPI);
 	}
 
-	private native boolean nativeOnWillPay(int cxxThis, String s, T t, MobPayAPI mobPayAPI);
-	private native void nativeOnPayEnd(int cxxThis, int payResult, T t, MobPayAPI mobPayAPI);
+	private native boolean nativeOnWillPay(int cxxThis, String s, Order t, MobPayAPI mobPayAPI);
+	private native void nativeOnPayEnd(int cxxThis, int payResult, Order t, MobPayAPI mobPayAPI);
 }
